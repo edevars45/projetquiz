17a159e7779a85d901c06302a124b53b5de5ccab
sofiane <sofianeagoumellah1@gmail.com>
2025-05-14 11:52:54 +0200

    suite de l'élaboration du projet avec ma collègue Esther

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,1 +1,3 @@
 // ce fichier permettra de charger les questions depuis le fichier JSON, affichera les options sous forme de boutons radio et calculera le score une fois le quiz soumis 
+const questions = await fetch ("dev.json");
+const reponses = await 
